275b7b6dc549691426133a4db82795dafe5363bb
nspeer12 <nspeer252@gmail.com>
2018-10-17 10:48:02 -0400

    set up project

---
+++ src/Rocket.java
@@ -0,0 +1,18 @@
+import java.awt.event.KeyEvent;
+
+import blobz.BlobAction;
+import blobz.PolyBlob;
+
+public class Rocket extends PolyBlob implements BlobAction{
+	
+	public Rocket(int x, int y)
+	{
+		super(x, y, 0);
+	}
+
+	@Override
+	public void keyAction(KeyEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/RocketTest.java
@@ -0,0 +1,9 @@
+
+public class RocketTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Rockety Rocket");
+	}
+
+}
