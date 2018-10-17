dbf5c654207acc920f1634ebc31c75aacc310e76
nspeer12 <nspeer252@gmail.com>
2018-10-17 11:26:42 -0400

    created classes and created points for rocket

--- src/Rocket.java
+++ src/Rocket.java
@@ -1,3 +1,4 @@
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 
 import blobz.BlobAction;
@@ -5,11 +6,24 @@ import blobz.PolyBlob;
 
 public class Rocket extends PolyBlob implements BlobAction{
 	
+	Point[] p = {
+			new Point(0,0),
+			new Point(5,-10),
+			new Point(15,0),
+			new Point(0,15),
+			new Point(-15,0),
+			new Point(-5,-10),
+			
+	};
+	
 	public Rocket(int x, int y)
 	{
 		super(x, y, 0);
+		super.setPolygon(p);
 	}
-
+	
+	
+	
 	@Override
 	public void keyAction(KeyEvent arg0) {
 		// TODO Auto-generated method stub
--- src/RocketTest.java
+++ src/RocketTest.java
@@ -1,9 +1,36 @@
+import java.awt.Color;
 
-public class RocketTest {
+import blobz.BlobGUI;
+import blobz.SandBox;
+import blobz.SandBoxMode;
 
+public class RocketTest implements BlobGUI{
+
+	
+	
+	static SandBox sandbox;
+	
+	public RocketTest()
+	{
+		sandbox = new SandBox(Color.BLACK);
+		sandbox.init((BlobGUI) this);
+		sandbox.setSandBoxMode(SandBoxMode.FLOW);
+		sandbox.setFrameRate(100);
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Rockety Rocket");
+		
+		new RocketTest();
+	}
+	
+	public void generate()
+	{
+		Rocket rocket = new Rocket(1,1);
+		rocket.setLoc(100, 100);
+		rocket.setColor(Color.WHITE);
+		System.out.println(rocket.report());
+		sandbox.addBlob(rocket);
 	}
 
 }
